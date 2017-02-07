a7ca429dee042e9960873701e094af68405d0730
pfZone <zhpfxl@sina.com>
2017-02-07 16:19:43 +0800

    Add files via upload

---
+++ sshCmd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import paramiko
+import threading
+
+def ssh2(ip,username,passwd,cmd):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip,22,username,passwd,timeout = 3)
+        for m in cmd:
+            stdin,stdout,stderr = ssh.exec_command(m)
+            stdin.write("Y")
+            out = stdout.readlines()
+            for o in out:
+                print o,
+        #print '%s\tok\n'%(ip)
+        ssh.close()
+    except:
+        print '%s\tError'%(ip)
+
+
+if __name__ == '__main__':
+    hosts = {}
+    hosts = {
+                'IP0':"192.168.180.128",
+                'IP1':"192.168.181.128",
+                'IP2':"192.168.182.128",
+                'IP3':"192.168.183.128",
+            }
+    cmd = ['cal','uname -a']
+    username = "root"
+    passwd = "123456"
+    threads = []
+    for ip in hosts.values():
+        a = threading.Thread(target=ssh2,args=(ip,username,passwd,cmd))
+        #ssh2(ip,username,passwd,cmd)
+        a.start()
